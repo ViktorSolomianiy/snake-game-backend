a489245d70890a24267d59d9a10930da87124598
ViktorSolomianiy <viktorsolomianiy@gmail.com>
2023-10-30 16:48:58 +0200

    test

--- db.js
+++ db.js
@@ -1,16 +1,29 @@
 const { Client } = require("pg");
 
-const client = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "snake_game",
-  password: "petproj",
-  port: 5432,
-});
+const connectToDB = () => {
+  const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "snake_game",
+    password: "petproj",
+    port: 5432,
+  });
 
-client
-  .connect()
-  .then(() => console.log("Connected to the database"))
-  .catch((err) => console.error("Error connecting to the database", err));
+  client
+    .connect()
+    .then(() => console.log("Connected to the database"))
+    .catch((err) => {
+      console.error("Error connecting to the database", err);
+
+      setTimeout(() => {
+        console.log("Retrying connection...");
+        connectToDB();
+      }, 5000);
+    });
+
+  return client;
+};
+
+const client = connectToDB();
 
 module.exports = client;
