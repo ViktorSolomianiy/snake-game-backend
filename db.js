9417fc16555e74660f16723bb5c5c50afb5f9da8
ViktorSolomianiy <viktorsolomianiy@gmail.com>
2023-10-30 16:34:44 +0200

    test

--- db.js
+++ db.js
@@ -13,7 +13,18 @@ client
   .then(() => console.log("Connected to the database"))
   .catch((err) => {
     console.error("Error connecting to the database", err);
-    setTimeout(console.log("hello"), 5000);
+    setTimeout(
+      console.log(
+        client
+          .connect()
+          .then(() => console.log("Connected to the database"))
+          .catch((err) => {
+            console.error("Error connecting to the database", err);
+            setTimeout(console.log("hello"), 5000);
+          })
+      ),
+      5000
+    );
   });
 
 module.exports = client;
