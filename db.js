16228a086a0d928bbae89b424207d05f4f04fa78
ViktorSolomianiy <viktorsolomianiy@gmail.com>
2023-10-31 10:35:24 +0200

    test

--- db.js
+++ db.js
@@ -49,8 +49,15 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  // ssl: true,
+  ssl: true,
 });
+
+pool
+  .connect()
+  .then(() => console.log("Connected to the database"))
+  .catch((err) => {
+    console.error("Error connecting to the database", err);
+  });
 
 // const pool = connectToDB();
 
